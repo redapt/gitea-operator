e1b600e3d5f4883216c28df54ad40617558869ee
Paul Welch <paulwelch@outlook.com>
2019-08-15 16:46:48 -0700

    initial checkin
    initial checkin

---
+++ README.md
@@ -0,0 +1,4 @@
+# gitea-operator
+
+This project is an example kubernetes operator for a popular open source version control tool, Gitea (see https://gitea.io).
+
---
+++ pkg/apis/addtoscheme_redapt_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"github.com/redapt/gitea-operator/pkg/apis/redapt/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
